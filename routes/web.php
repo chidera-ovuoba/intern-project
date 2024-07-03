fb6ae68a21a6facb788261f560fcf1e7eb81b604
Ovuoba Chidera Nkwuda <ovuobachidera18@gmail.com>
2024-07-03 22:48:23 +0100

    define an API route for creating orders

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/orders','App\Http\Controllers\OrderController@store');
